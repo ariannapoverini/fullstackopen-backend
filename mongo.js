a64914e4ab3c9b52176f245022a22c29bf565b9c
Arianna Poverini <arianna.poverini@gmail.com>
2022-05-09 23:08:47 +0200

    Exercise 3.12: Command-line database

---
+++ mongo.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log(
+    "Please provide the password as an argument: node mongo.js <password>"
+  );
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://ariannapoverini:${password}@artohellayeah.vts50.mongodb.net/noteApp?retryWrites=true&w=majority`;
+
+mongoose.connect(url);
+
+const phoneSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+  date: Date,
+});
+
+const Person = mongoose.model("Person", phoneSchema);
+
+if (process.argv.length > 3) {
+  // no need for a loop
+  const name = process.argv[3];
+  const number = process.argv[4];
+
+  const person = new Person({
+    name,
+    number,
+    date: new Date(),
+  });
+
+  person.save().then((result) => {
+    console.log(`Added person ${result.name} and number ${result.number}`);
+    mongoose.connection.close();
+  });
+} else if (process.argv.length === 3) {
+  Person.find({}).then((result) => {
+    result.forEach((person) => {
+      console.log(`${person.name} ${person.number}`);
+    });
+    mongoose.connection.close();
+  });
+}
