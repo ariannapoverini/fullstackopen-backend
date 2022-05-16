619a359b3dbd68cffaac4ddd158f4c20188fd528
Arianna Poverini <arianna.poverini@gmail.com>
2022-05-16 10:29:55 +0200

    Exercises 3.19 - 3.20 Phonebook database

--- models/phonebook.js
+++ models/phonebook.js
@@ -23,8 +23,13 @@ const phoneSchema = new mongoose.Schema({
   },
   number: {
     type: String,
-    minLength: 8,
-    required: true,
+    validate: {
+      validator: function (v) {
+        return /\d{2,3}-\d{3,8}/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid phone number`,
+    },
+    required: [true, "User phone number required"],
   },
   date: {
     type: Date,
